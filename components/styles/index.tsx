bb72edde113e327e238938efb777fb21bfad0a2a
TingzhouJia <joe.tzjia@gmail.com>
2020-10-03 15:39:57 -0400

    button setup

---
+++ components/styles/index.tsx
@@ -0,0 +1,2 @@
+export const primary="linear-gradient(0deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1)), linear-gradient(88.48deg, #FF5A7A 3.58%, #FF9C9C 79.83%)"
+export const secondary="#ffffff"
