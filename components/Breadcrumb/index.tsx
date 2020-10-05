6c22405132ce2b248e3c33f7a2041139268eca59
TingzhouJia <joe.tzjia@gmail.com>
2020-10-05 14:58:05 -0400

    :rocket: breadscrumb done

---
+++ components/Breadcrumb/index.tsx
@@ -0,0 +1,5 @@
+import Breadscrumb,{BreadcrumbProps} from './breadcrumb'
+export {BreadSeperator} from './bread-seperator'
+export {BreadcrumbItemProps} from './breadcrumb-item'
+export {BreadcrumbProps}
+export default Breadscrumb
