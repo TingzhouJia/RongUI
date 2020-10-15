9979cbef380fa5d7ca55e8057c0ebec57952ae7c
TingzhouJia <joe.tzjia@gmail.com>
2020-10-15 10:47:26 -0400

    tooltip init

---
+++ components/Tooltip/wrapper.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const TooltipBase=styled.div`
+width: max-content;
+          display: inline-block;
+`
+export const TooltopContentBase=styled.div``
+
