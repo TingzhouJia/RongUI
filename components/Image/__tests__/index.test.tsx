5f91deddf0209790a509386916a690407c018592
TingzhouJia <joe.tzjia@gmail.com>
2020-10-31 22:28:49 -0400

    :fire: feat:(add image snapshot)

--- components/Image/__tests__/index.test.tsx
+++ components/Image/__tests__/index.test.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import 'jest-styled-components'
 import {mount} from 'enzyme'
 import Image from '../index'
+import renderer from 'react-test-renderer'
 describe('Image Test', () => {
     it("should render image",()=>{
         const wrapper=mount(<Image src="https://images.unsplash.com/photo-1602524206684-fdf6393c7d89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"/>)
@@ -15,5 +16,9 @@ describe('Image Test', () => {
 
     })
 
+    it("should have snapshot",()=>{
+        const wrapper=renderer.create(<Image src="https://images.unsplash.com/photo-1602524206684-fdf6393c7d89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=8"/>).toJSON()
+        expect(wrapper).toMatchSnapshot()
+    })
     
 })
