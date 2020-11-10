d3f077ca49384f7285bbcfba0beb2b63e568e7e0
TingzhouJia <joe.tzjia@gmail.com>
2020-11-09 23:30:48 -0500

    :art: add light theme

--- components/styles/design.ts
+++ components/styles/design.ts
@@ -1,4 +1,4 @@
-import { ThemesExpressiveness,ThemesBreakpoints, ThemeFontType, ThemesFont } from "../utils"
+import { ThemesExpressiveness, ThemeFontType, ThemesFont, PaddingRule, MarginRule } from "../utils"
 
 export const expressiveness:ThemesExpressiveness = {
     linkStyle: 'none',
@@ -7,7 +7,8 @@ export const expressiveness:ThemesExpressiveness = {
     shadowSmall: '0px 6px 16px -8px rgba(0,0,0,0.08), 0px 9px 28px 0px rgba(0,0,0,0.05), 0px 12px 48px 16px rgba(0,0,0,0.03)',
     shadowMedium: '0px 3px 6px -4px rgba(0,0,0,0.12), 0px 6px 16px 0px rgba(0,0,0,0,08), 0px 9px 28px 8px rgba(0,0,0,0.05)',
     shadowLarge: '0 1px 2px -2px rgba(0, 0, 0, 0.16),0px 3px 6px 0px rgba(0,0,0,0.12),0px 5px 12px 4px rgba(0,0,0,0.09)',
-    portalOpacity: 0.25,
+    borderRadius:"2px",
+    maskColor:"rgba(0,0,0,.45)"
   }
 
   export const defaultFont:ThemesFont = {
@@ -18,6 +19,11 @@ export const expressiveness:ThemesExpressiveness = {
   }
 
   export const defaultFontSize:ThemeFontType={
+      small:{
+        size:"12px",
+        height:"20",
+        weight:"400"
+      },
       normal:{
           size:'14px',
           height:'22px',
@@ -29,7 +35,7 @@ export const expressiveness:ThemesExpressiveness = {
           weight:'400'
       }
   }
-  export const defaultBreakpoints:ThemesBreakpoints = {
+  export const defaultBreakpoints = {
     xs: {
       min: '0',
       max: '650px',
@@ -51,3 +57,15 @@ export const expressiveness:ThemesExpressiveness = {
       max: '10000px',
     },
   }
+export const defaultPadding:PaddingRule={
+    sm:"12px",
+    md:"16px",
+    lg:"24px"
+}
+
+export const defaultMargin:MarginRule={
+  sm:"12px",
+  md:"16px",
+  lg:"24px"
+}
+
