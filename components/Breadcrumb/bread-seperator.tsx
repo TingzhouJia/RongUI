9634397ea7802be123fa8fa9886c798ce9a5a7fe
TingzhouJia <joe.tzjia@gmail.com>
2020-11-11 15:11:15 -0500

    :art: revise breadcrumbs ui

--- components/Breadcrumb/bread-seperator.tsx
+++ components/Breadcrumb/bread-seperator.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import styled from 'styled-components'
+import { SeperatorWrapper } from './wrapper'
+
 export interface BreadSeperator {
     className?:string
 
 }
 
-const SeperatorWrapper=styled.span`
-         display: inline-flex;
-          margin: 0 8px;
-          user-select: none;
-          pointer-events: none;
-          color: rgba(0,0,0,0.45);
-          align-items: center;
-`
+
 const BreadSeperator:React.FC<BreadSeperator>=({children,className})=>{
     return <SeperatorWrapper id="bread-seperator" className={className}>{children||'/'}</SeperatorWrapper>
 }
--- components/Breadcrumb/breadcrumb.tsx
+++ components/Breadcrumb/breadcrumb.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 import BreadSeperator from "./bread-seperator"
 import BreadcrumbItem from "./breadcrumb-item";
-import styled from "styled-components";
+import styled, { CSSProperties } from "styled-components";
+import { Nav } from "./wrapper";
 
-export interface Route {
+
+interface Route {
   path: string;
   breadcrumbName: string;
   children?: Omit<Route, 'children'>[];
@@ -13,81 +15,43 @@ export interface BreadcrumbProps {
 
   separator?: React.ReactNode;
   className?: string;
-}
-function getBreadcrumbName(route: Route, params: any) {
-  if (!route.breadcrumbName) {
-    return null;
-  }
-  const paramsKeys = Object.keys(params).join('|');
-  const name = route.breadcrumbName.replace(
-    new RegExp(`:(${paramsKeys})`, 'g'),
-    (replacement, key) => params[key] || replacement,
-  );
-  return name;
+  style?:CSSProperties
 }
 
-function defaultItemRender(route: Route, params: any, routes: Route[], paths: string[]) {
-  const isLastItem = routes.indexOf(route) === routes.length - 1;
-  const name = getBreadcrumbName(route, params);
-  return isLastItem ? <span>{name}</span> : <a href={`#/${paths.join('/')}`}>{name}</a>;
-}
 
-const getPath = (path: string, params: any) => {
-  path = (path || '').replace(/^\//, '');
-  Object.keys(params).forEach(key => {
-    path = path.replace(`:${key}`, params[key]);
-  });
-  return path;
-};
 
-const Nav = styled.nav`
-      margin: 0;
-          padding: 0;
-          line-height: inherit;
-          color: #d9d9d9;
-          font-size: 14px;
-          box-sizing: border-box;
-          display: flex;
-          align-items: center;
 
-          & > span:last-child {
-    color: black;
-    opacity:0.85;
-    a {
-      color: ${props => props.theme.colors.primary};
-      cursor:pointer;
-    }
-  }
-`
 interface BreadcrumbInterface extends React.FC<BreadcrumbProps> {
   Item: typeof BreadcrumbItem;
   Separator: typeof BreadSeperator;
 }
 
-const Breadcrumb: BreadcrumbInterface = ({ separator = "/", className, children
+const Breadcrumb: BreadcrumbInterface = ({ separator = "/", className, children,style
 }) => {
+
   const childrenArray = React.Children.toArray(children)
+
 
   const withSeparatorChildren = childrenArray.map((item, index) => {
     if (!React.isValidElement(item)) return item
-    const last = childrenArray[index - 1]
-    const lastIsSeparator = React.isValidElement(last) && last.type === BreadSeperator
-    const currentIsSeparator = item.type === BreadSeperator
-    if (!lastIsSeparator && !currentIsSeparator && index > 0) {
+    if (index > 0) {
       return (
         <React.Fragment key={index}>
           <BreadSeperator >{separator}</BreadSeperator>
-          {item}
+          {
+            React.cloneElement(item,{key:`rong-breadcrumb-item-${index}`,})
+          }
+          
         </React.Fragment>
       )
     }
-    return item
+    return React.cloneElement(item,{key:`rong-breadcrumb-item-${index}`})
   })
   let crumbs
   if (children) {
     crumbs = withSeparatorChildren
   }
-  return (<Nav className={className}>
+  return (<Nav className={className} style={style}>
     {crumbs}
   </Nav>)
 
