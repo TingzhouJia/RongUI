e4b66824b954027f58b6634e84c3fd7a31d76a4f
TingzhouJia <joe.tzjia@gmail.com>
2020-11-14 13:04:11 -0500

    :memo: add breadcrumb story

---
+++ src/stories/breadcrumbs.stories.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Breadcrumb } from '../../components'
+import { themeIt } from './utils/withTheme'
+
+export default {
+    title: 'Navigation / Breadcrumb',
+    component: Breadcrumb as any,
+    decorators: [themeIt],
+    parameters: {
+        docs: {
+            description: ''
+        }
+    },
+    argTypes: {
+
+    },
+
+};
+
+export const Basic=()=>{
+    return (
+        <Breadcrumb >
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>Rong</Breadcrumb.Item>
+        <Breadcrumb.Item>UI</Breadcrumb.Item>
+        </Breadcrumb>
+    )
+}
+
+export const CustomizeSeperator=()=>{
+    return (
+        <Breadcrumb separator="%">
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>Rong</Breadcrumb.Item>
+        <Breadcrumb.Item>UI</Breadcrumb.Item>
+        </Breadcrumb>
+    )
+}
