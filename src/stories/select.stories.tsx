5b482cc04e9f5ca0bb1f0d24283bde26fa739c4b
TingzhouJia <joe.tzjia@gmail.com>
2020-11-16 12:17:36 -0500

    :memo: select story

---
+++ src/stories/select.stories.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import { Select } from '../../components'
+import { themeIt } from './utils/withTheme'
+
+
+export default {
+    title: 'Data Collection / Select',
+    component: Select as any,
+    decorators: [themeIt],
+    parameters: {
+        docs: {
+            description: ''
+        }
+    },
+    argTypes: {
+
+    },
+
+};
+
+export const Basic = () => {
+    return (
+        <Select>
+            <Select.Option value="Rong">
+                Rong UI 1
+            </Select.Option>
+            <Select.Option value="Rong2">
+                Rong UI 2
+            </Select.Option>
+        </Select>
+    )
+}
+
+export const Multiple = () => {
+    return (
+        <Select multiple>
+            <Select.Option value="Rong">
+                Rong UI 1
+            </Select.Option>
+            <Select.Option value="Rong2">
+                Rong UI 2
+            </Select.Option>
+        </Select>
+    )
+}
+
+export const Group = () => {
+    return (
+        <Select multiple>
+            <Select.OptGroup label="group">
+                <Select.Option value="Rong">
+                    Rong UI 1
+        </Select.Option>
+            </Select.OptGroup>
+            <Select.Option value="Rong2">
+                Rong UI 2
+        </Select.Option>
+        </Select>
+    )
+}
