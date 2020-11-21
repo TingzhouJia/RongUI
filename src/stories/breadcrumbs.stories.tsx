fe34f21ce9de369b1126efd925f5624dfc5b428b
TingzhouJia <joe.tzjia@gmail.com>
2020-11-21 12:55:06 -0500

    :memo: revise breadcrumb docs

--- src/stories/breadcrumbs.stories.tsx
+++ src/stories/breadcrumbs.stories.tsx
@@ -7,12 +7,53 @@ export default {
     component: Breadcrumb as any,
     decorators: [themeIt],
     parameters: {
-        docs: {
-            description: ''
+        componentSubtitle: "A breadcrumb displays the current location in wegbpage.",
+        docs:{
+            description:{
+                component:"<h3>When To Use?</h3><br/><ul><li>When the system has more than two nodes in a route.</li><li>When you need to inform the user of where they are.</li><li>When the user may need to navigate back to parent level</li></ul>"
+                ,
+                
+            },
+            source:{
+                type:"code"
+            }
         }
     },
     argTypes: {
-
+        style: {
+            description: "The style object of container<br/><h6>type:</h6>",
+            table:{
+                type:{
+                    summary:"CSSProperties"
+                }
+            },
+            control:{}
+        },
+        className: {
+            description: "The className object of container<br/><h6>type:</h6>",
+            table:{
+                type:{
+                    summary:"string"
+                }
+            },
+        },
+        seperator:{
+            description:"separator to divide each router<br/><h6>type:</h6>",
+            table: {
+                type:{
+                    summary:"ReactNode"
+                },
+                defaultValue: { summary: "/" }
+            }
+        },
+        href:{
+            description:"link version BreacrumbItem<br/><h6>type:</h6>",
+            table:{
+                type:{
+                    summary:"string"
+                }
+            }
+        }
     },
 
 }
