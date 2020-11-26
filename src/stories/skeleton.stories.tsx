1a40949e5c4703218fffd778fb7fb96446646486
TingzhouJia <joe.tzjia@gmail.com>
2020-11-26 18:13:02 -0500

    :memo: skeleton docs

--- src/stories/skeleton.stories.tsx
+++ src/stories/skeleton.stories.tsx
@@ -8,12 +8,97 @@ export default {
     component: Skeleton as any,
     decorators: [themeIt],
     parameters: {
+        componentSubtitle:"Provide a placeholder while waiting for content to load.",
         docs: {
-            description: ''
+            description: {
+                component:"<h3>When To Use?</h3><ul><li>When a resource needs long time to load.</li><li>When the component contains lots of information, such as List or Card.</li></ul>"
+            }
         }
     },
     argTypes: {
-
+        size: {
+            description: "The size of input<h6>type</h6>",
+            table: {
+                type: {
+                    summary: '"small" | "default" | "large"'
+                },
+                defaultValue: {
+                    summary: "default"
+                }
+            }
+        },
+        style: {
+            description: "The  style object of container<br/><h6>type:</h6>",
+            table: {
+                type: {
+                    summary: "CSSProperties"
+                }
+            }
+        },
+        className: {
+            description: "The className object of container<br/><h6>type:</h6>",
+            table: {
+                type: {
+                    summary: "string"
+                }
+            }
+        },
+        paragraph:{
+            description:"Add paragraph block to Skeleton.<h6>type:</h6>",
+            table:{
+                type:{
+                    summary:" {row:number,active?:boolean, width?:number} | boolean "
+                },
+                defaultValue:{
+                    summary:"{row:3}"
+                }
+            },
+        
+        },
+        title:{
+            description:"Add title block to Skeleton.<h6>type:</h6>",
+            table:{
+                type:{
+                    summary:"boolean "
+                },
+                defaultValue:{
+                    summary:"false"
+                }
+            }
+        },
+        avatar:{
+            description:"Add avatar block to Skeleton.<h6>type:</h6>",
+            table:{
+                type:{
+                    summary:"boolean "
+                },
+                defaultValue:{
+                    summary:"false"
+                }
+            }
+        },
+        loading:{
+            description:"If loading, skeleton will show<h6>type:</h6>",
+            table:{
+                type:{
+                    summary:"boolean"
+                },
+                defaultValue:{
+                    summary:"true"
+                }
+            }
+        },
+        acitve:{
+            description:"Animation of skeleton<h6>type:</h6>",
+            table:{
+                type:{
+                    summary:"boolean"
+                },
+                defaultValue:{
+                    summary:"false"
+                }
+            }
+        },
     },
 
 };
